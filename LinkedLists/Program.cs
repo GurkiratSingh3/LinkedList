5ccf03b2b1bc9bea03611e2af3dc3893392b1a9c
Gurkirat Singh <c0731779@mylambton.ca>
2019-02-20 19:43:42 -0500

    LinkedLists

--- LinkedLists/Program.cs
+++ LinkedLists/Program.cs
@@ -10,6 +10,25 @@ namespace LinkedLists
     {
         static void Main(string[] args)
         {
+            Node myNode = new Node(7);
+            myNode.next = new Node(5);
+            myNode.print();
+        }
+    }
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int i)
+        {
+            data = i;
+            next = null;
+        }
+        public void print()
+        {
+            Console.WriteLine("| " + data + " | ->");
+            Console.ReadLine();
         }
     }
 }
